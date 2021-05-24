4e63f0cbe91c48cfb77fc9ae9bc5fb2ff99ec9d4
ex0rb <ex0rb@outlook.com>
2021-05-24 18:19:37 +0200

    i forgot to set the random seed

--- pipenoise.c
+++ pipenoise.c
@@ -1,39 +1,11 @@
-/*
-    This is free and unencumbered software released into the public domain.
-    
-    Anyone is free to copy, modify, publish, use, compile, sell, or
-    distribute this software, either in source code form or as a compiled
-    binary, for any purpose, commercial or non-commercial, and by any
-    means.
-    
-    In jurisdictions that recognize copyright laws, the author or authors
-    of this software dedicate any and all copyright interest in the
-    software to the public domain. We make this dedication for the benefit
-    of the public at large and to the detriment of our heirs and
-    successors. We intend this dedication to be an overt act of
-    relinquishment in perpetuity of all present and future rights to this
-    software under copyright law.
-    
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-    IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-    OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-    OTHER DEALINGS IN THE SOFTWARE.
-    
-    For more information, please refer to <https://unlicense.org>
-*/
-
+#include <time.h>
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
 #include <stdlib.h>
 
 /*
-    pipenoise.c
-        Reads a stream from STDIN, damages it by the given (or default)
-        probability, then writes it into STDOUT.
+    pipenoise.c - Damages data streams to test error correction.
 */
 
 #ifndef DEFAULT_PROB
@@ -60,6 +32,8 @@ int main(int argc, char *argv[])
     const char *write_off;
     ssize_t avail_num, write_num, iter;
     float prob = DEFAULT_PROB;
+
+    srand(time(NULL));
 
     /* arguments */
     if (argc > 1)
